c24376da7517dbfa66925196c65950d6209dcaef
AndrewGrabowski2024 <819788@mystma.org>
2021-10-05 14:54:39 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 #WRITE YOUR CODE IN THIS FILE
+def hasL(w):
+    i = 0
+    for i in range(0, len(w)):
+        if w[i] == "l":
+            return True
+    else:
+         return False
+
+print(hasL("meon"))
